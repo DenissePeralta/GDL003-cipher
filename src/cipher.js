809afd86e6cc8ba7e361c9b2be3689056fc530b4
peraltadnni <dnisse-x00@hotmail.com>
2019-06-03 23:31:38 -0500

    Funcion para el boton comenzar

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,11 @@
 window.cipher = {
-  // ... 
+  // ...
 };
+document.getElementById("pagina-principal").style.display = "block";
+document.getElementById("pagina-secundaria").style.display = "none";
+
+//Esta función es para pasar a la siguiente pantalla
+function comenzar() {
+  document.getElementById("pagina-principal").style.display = "none";
+  document.getElementById("pagina-secundaria").style.display = "block";
+}
